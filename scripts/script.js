14291740948ff4d61ce87548a3f6aa7c77df0a51
Vinícius Nunes de Oliveira <br.viniciusnunes@gmail.com>
2025-07-01 20:05:46 -0300

    chore: minor changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const urlSite = document.getElementById("url-site");
 const btnCopiarTexto = document.getElementById("copiar-texto");
 
-urlSite.textContent = window.location.href + "[codigo]";
+urlSite.textContent = window.location.href + "/[codigo]";
 btnCopiarTexto.addEventListener("click", copiarTexto);
 
 function copiarTexto() {
