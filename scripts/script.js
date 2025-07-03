fc6b57ee55545f99e0f9795e9e340f9b297c0ce8
Vinícius Nunes de Oliveira <br.viniciusnunes@gmail.com>
2025-07-03 05:08:09 -0300

    fix(script): remove .html from URL

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
 const urlSite = document.getElementById("url-site");
 const btnCopiarTexto = document.getElementById("copiar-texto");
 
-urlSite.textContent = window.location.host + "/[codigo]";
+urlSite.textContent = (window.location.href + "[codigo]").replace(
+  "index.html",
+  "",
+);
+
 btnCopiarTexto.addEventListener("click", copiarTexto);
 
 function copiarTexto() {
